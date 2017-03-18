4670f48dec4c21f471689b51f96740f1ea117870
Adam Wills <heavymetaladam@gmail.com>
2017-03-18 16:21:09 -0400

    Using npms version of prism

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,8 @@ require("prismjs/components/prism-core.js");
 const lazyLoad = require("vanilla-lazyload");
 
 require("prismjs/components/prism-markup.js");
+require("prismjs/components/prism-bash.js");
+require("prismjs/components/prism-clike.js");
+require("prismjs/components/prism-javascript.js");
 
 const myLazyLoad = new lazyLoad();
